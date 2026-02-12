2620f1a772e147a4108a02994fa97b1fbcc2a7b0
GitHub Action <action@github.com>
2026-02-12 23:10:06 +0000

    deploy: fjmartincampo/fjmartincampo.github.io@9171e93605efce30a2219b848961a4a2156bde9e

--- spanish/assets/js/search-data.js
+++ spanish/assets/js/search-data.js
@@ -1,7 +1,7 @@
 
 const currentUrl = window.location.href;
-const siteUrl = "https://fjmartincampo.github.io"; 
-let updatedUrl = currentUrl.replace("https://fjmartincampo.github.io", "");
+const siteUrl = "https://www.fjmartincampo.com"; 
+let updatedUrl = currentUrl.replace("https://www.fjmartincampo.com", "");
 if (currentUrl.length == updatedUrl.length && currentUrl.startsWith("http://127.0.0.1")) {
   const otherSiteUrl = siteUrl.replace("localhost", "127.0.0.1");
   updatedUrl = currentUrl.replace(otherSiteUrl + "", "");
